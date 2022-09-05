bb4f66579ff4bc563ee0ffc0254d12d549dc328e
eurorondon03 <eurorondon03@gmail.com>
2022-09-04 23:00:16 -0400

    finalizado capitulo llamado -> CARD SCREEN

--- src/redux/Actions/cartActions.js
+++ src/redux/Actions/cartActions.js
@@ -17,7 +17,7 @@ export const addToCart = (id, qty) => async (dispatch, getState) => {
       name: data.name,
       image: data.image,
       price: data.price,
-      countInStock: data.countInStock,
+      countInStock: data.countinStock,
       qty,
     },
   });
