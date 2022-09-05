682f38f338a899904288b5494f847500998bf93a
eurorondon03 <eurorondon03@gmail.com>
2022-09-05 16:40:37 -0400

    finalizado capitulo llamado -> REGISTER SCREEN

--- src/redux/Actions/userActions.js
+++ src/redux/Actions/userActions.js
@@ -1,10 +1,21 @@
-import { USER_LOGIN_FAIL,
-   USER_LOGIN_REQUEST,
-    USER_LOGIN_SUCCESS, 
-    USER_LOGOUT
+import {
+   USER_DETAILS_FAIL,
+  USER_DETAILS_REQUEST,
+  USER_DETAILS_RESET,
+  USER_DETAILS_SUCCESS,
+  USER_LOGIN_FAIL,
+  USER_LOGIN_REQUEST,
+  USER_LOGIN_SUCCESS,
+  USER_LOGOUT,
+  USER_REGISTER_FAIL,
+  USER_REGISTER_REQUEST,
+  USER_REGISTER_SUCCESS,
+  USER_UPDATE_PROFILE_FAIL,
+  USER_UPDATE_PROFILE_REQUEST,
+  USER_UPDATE_PROFILE_SUCCESS,
   } from "../Constants/UserConstants";
   import axios from "axios";
-import { USER_DETAILS_RESET } from './../Constants/UserConstants';
+
 
 
 // LOGIN
@@ -42,4 +53,37 @@ export const logout = () => (dispatch) => {
   document.location.href= "/login";
   //dispatch({ type: USER_DETAILS_RESET });
  // dispatch({ type: ORDER_LIST_MY_RESET });
+};
+
+
+
+// REGISTER
+export const register = (name, email, password) => async (dispatch) => {
+  try {
+    dispatch({ type: USER_REGISTER_REQUEST});
+
+    const config = {
+      headers: {
+        "Content-Type": "application/json",
+      },
+    };
+
+    const { data } = await axios.post(
+      `/api/users`,
+      { name, email, password },
+      config
+    );
+    dispatch({ type: USER_REGISTER_SUCCESS, payload: data });
+    dispatch({ type: USER_LOGIN_SUCCESS, payload: data });
+
+    localStorage.setItem("userInfo", JSON.stringify(data));
+  } catch (error) {
+    dispatch({
+      type: USER_REGISTER_FAIL,
+      payload:
+        error.response && error.response.data.message
+          ? error.response.data.message
+          : error.message,
+    });
+  }
 };
